2fe013f045f3fc4da88d7aab536edbf180de447f
Kevin Chan <kvii.chan@gmail.com>
2015-03-10 00:02:06 -0400

    Added moment for broadcasted messages

--- routes/socket.js
+++ routes/socket.js
@@ -67,7 +67,8 @@ module.exports = function (socket) {
   socket.on('send:message', function (data) {
     socket.broadcast.emit('send:message', {
       user: name,
-      text: data.message
+      text: data.message,
+      time: new Date()
     });
   });
 
